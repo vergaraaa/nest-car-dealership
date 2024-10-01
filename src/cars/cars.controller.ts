c303a3ca7a2287401051ee8b1783fbf3cdea7d1e
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-01 06:18:09 +0200

    ✨ create car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -27,7 +27,7 @@ export class CarsController {
 
   @Post()
   createCar(@Body() createCarDto: CreateCarDto) {
-    return createCarDto;
+    return this.carsService.create(createCarDto);
   }
 
   @Patch(':id')
