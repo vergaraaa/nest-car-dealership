dd179ff3f939d53806659fe132a30a2c02086343
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-03 06:28:15 +0200

    ✨ populate seed endpoint and update readme

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from 'src/brands/brands.service';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly brandsService: BrandsService,
+    private readonly carsService: CarsService,
+  ) {}
+
   populateDB() {
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+
     return `Seeds executed`;
   }
 }
